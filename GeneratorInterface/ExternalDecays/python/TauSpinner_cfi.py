df74cb035eea0a60c6a03fe40149ee8b36da624e
inugent <you@somedomain.com>
2013-11-21 19:00:19 +0100

    patch for randomvariable check in 7_0_x

--- GeneratorInterface/ExternalDecays/python/TauSpinner_cfi.py
+++ GeneratorInterface/ExternalDecays/python/TauSpinner_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 TauSpinnerReco = cms.EDProducer( "TauSpinnerCMS",
                                  isReco = cms.bool(True),
                                  isTauolaConfigured = cms.bool(False),
-                                 isLHPDFConfigured = cms.bool(True),
+                                 isLHPDFConfigured = cms.bool(False),
                                  LHAPDFname = cms.untracked.string('MSTW2008nnlo90cl.LHgrid'),
                                  CMSEnergy = cms.double(8000.0),
                                  gensrc = cms.InputTag('genParticles')
