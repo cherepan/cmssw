f437bd7416ca755058f0a0279767575548776247
inugent <you@somedomain.com>
2014-03-05 21:15:27 +0100

    patches for photos generator

--- GeneratorInterface/PhotosInterface/plugins/PhotosppInterface.cc
+++ GeneratorInterface/PhotosInterface/plugins/PhotosppInterface.cc
@@ -40,38 +40,36 @@ void PhotosppInterface::configureOnlyFor( int ipdg ){
 void PhotosppInterface::init(){
   if ( fIsInitialized ) return; // do init only once
   Photospp::Photos::initialize();
-
   std::vector<std::string> par = fPSet->getParameter< std::vector<std::string> >("parameterSets");
   for (unsigned int ip=0; ip<par.size(); ++ip ){
     std::string curSet = par[ip];
-    edm::ParameterSet cfgAlgorithm = fPSet->getParameter<edm::ParameterSet>(curSet);   
     
     // Physics settings
-    if(curSet=="maxWtInterference")                Photospp::Photos::maxWtInterference(cfgAlgorithm.getParameter<double>(curSet));
-    if(curSet=="setInfraredCutOff")                Photospp::Photos::setInfraredCutOff(cfgAlgorithm.getParameter<double>(curSet));
-    if(curSet=="setAlphaQED")                      Photospp::Photos::setAlphaQED(cfgAlgorithm.getParameter<double>(curSet));
-    if(curSet=="setInterference")                  Photospp::Photos::setInterference(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="setDoubleBrem")                    Photospp::Photos::setDoubleBrem(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="setQuatroBrem")                    Photospp::Photos::setQuatroBrem(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="setExponentiation")                Photospp::Photos::setExponentiation(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="setCorrectionWtForW")              Photospp::Photos::setCorrectionWtForW(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="setMeCorrectionWtForScalar")       Photospp::Photos::setMeCorrectionWtForScalar(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="setMeCorrectionWtForW")            Photospp::Photos::setMeCorrectionWtForW(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="setMeCorrectionWtForZ")            Photospp::Photos::setMeCorrectionWtForZ(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="initializeKinematicCorrections")   Photospp::Photos::initializeKinematicCorrections(cfgAlgorithm.getParameter<int>(curSet));
-    if(curSet=="forceMassFrom4Vector")             Photospp::Photos::forceMassFrom4Vector(cfgAlgorithm.getParameter<bool>(curSet));
-    if(curSet=="forceMassFromEventRecord")         Photospp::Photos::forceMassFromEventRecord(cfgAlgorithm.getParameter<int>(curSet));
-    if(curSet=="ignoreParticlesOfStatus")          Photospp::Photos::ignoreParticlesOfStatus(cfgAlgorithm.getParameter<int>(curSet));
-    if(curSet=="deIgnoreParticlesOfStatus")        Photospp::Photos::deIgnoreParticlesOfStatus(cfgAlgorithm.getParameter<int>(curSet));
-    if(curSet=="setMomentumConservationThreshold") Photospp::Photos::setMomentumConservationThreshold(cfgAlgorithm.getParameter<double>(curSet));
-    if(curSet=="suppressAll") if(cfgAlgorithm.getParameter<bool>(curSet)==true)Photospp::Photos::suppressAll();
+    if(curSet=="maxWtInterference")                Photospp::Photos::maxWtInterference(fPSet->getParameter<double>(curSet));
+    if(curSet=="setInfraredCutOff")                Photospp::Photos::setInfraredCutOff(fPSet->getParameter<double>(curSet));
+    if(curSet=="setAlphaQED")                      Photospp::Photos::setAlphaQED(fPSet->getParameter<double>(curSet));
+    if(curSet=="setInterference")                  Photospp::Photos::setInterference(fPSet->getParameter<bool>(curSet));
+    if(curSet=="setDoubleBrem")                    Photospp::Photos::setDoubleBrem(fPSet->getParameter<bool>(curSet));
+    if(curSet=="setQuatroBrem")                    Photospp::Photos::setQuatroBrem(fPSet->getParameter<bool>(curSet));
+    if(curSet=="setExponentiation")                Photospp::Photos::setExponentiation(fPSet->getParameter<bool>(curSet));
+    if(curSet=="setCorrectionWtForW")              Photospp::Photos::setCorrectionWtForW(fPSet->getParameter<bool>(curSet));
+    if(curSet=="setMeCorrectionWtForScalar")       Photospp::Photos::setMeCorrectionWtForScalar(fPSet->getParameter<bool>(curSet));
+    if(curSet=="setMeCorrectionWtForW")            Photospp::Photos::setMeCorrectionWtForW(fPSet->getParameter<bool>(curSet));
+    if(curSet=="setMeCorrectionWtForZ")            Photospp::Photos::setMeCorrectionWtForZ(fPSet->getParameter<bool>(curSet));
+    if(curSet=="initializeKinematicCorrections")   Photospp::Photos::initializeKinematicCorrections(fPSet->getParameter<int>(curSet));
+    if(curSet=="forceMassFrom4Vector")             Photospp::Photos::forceMassFrom4Vector(fPSet->getParameter<bool>(curSet));
+    if(curSet=="forceMassFromEventRecord")         Photospp::Photos::forceMassFromEventRecord(fPSet->getParameter<int>(curSet));
+    if(curSet=="ignoreParticlesOfStatus")          Photospp::Photos::ignoreParticlesOfStatus(fPSet->getParameter<int>(curSet));
+    if(curSet=="deIgnoreParticlesOfStatus")        Photospp::Photos::deIgnoreParticlesOfStatus(fPSet->getParameter<int>(curSet));
+    if(curSet=="setMomentumConservationThreshold") Photospp::Photos::setMomentumConservationThreshold(fPSet->getParameter<double>(curSet));
+    if(curSet=="suppressAll")                      if(fPSet->getParameter<bool>(curSet)==true)Photospp::Photos::suppressAll();
   
   // Now setup more complicated radiation/mass supression and forcing.
     if(curSet=="suppressBremForBranch"){
-      std::vector<std::string> v = cfgAlgorithm.getParameter<std::vector<std::string> >(curSet);
+      edm::ParameterSet cfg = fPSet->getParameter<edm::ParameterSet>(curSet);
+      std::vector<std::string> v = cfg.getParameter<std::vector<std::string> >("parameterSets");
       for(unsigned int i=0;i<v.size();i++){
 	std::string vs = v[i];
-	edm::ParameterSet cfg = cfgAlgorithm.getParameter<edm::ParameterSet>(vs);
 	std::vector<int> vpar = cfg.getParameter<std::vector<int> >(vs);
 	if(vpar.size()==1)  Photospp::Photos::suppressBremForBranch(0,vpar[0]);
 	if(vpar.size()==2)  Photospp::Photos::suppressBremForBranch(0/*vpar[0]*/,vpar[1]);
@@ -85,29 +83,67 @@ void PhotosppInterface::init(){
 	if(vpar.size()==10) Photospp::Photos::suppressBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8],vpar[9]);
       }
     }
-    if(curSet=="forceBremForDecay"){
-      std::vector<std::string> v = cfgAlgorithm.getParameter<std::vector<std::string> >(curSet);
+    if(curSet=="suppressBremForDecay"){
+      edm::ParameterSet cfg = fPSet->getParameter<edm::ParameterSet>(curSet);
+      std::vector<std::string> v = cfg.getParameter<std::vector<std::string> >("parameterSets");
       for(unsigned int i=0;i<v.size();i++){
 	std::string vs = v[i];
-	edm::ParameterSet cfg = cfgAlgorithm.getParameter<edm::ParameterSet>(vs);
 	std::vector<int> vpar = cfg.getParameter<std::vector<int> >(vs);
-	if(vpar.size()==1)  Photospp::Photos::forceBremForDecay(0,vpar[0]);
-	if(vpar.size()==2)  Photospp::Photos::forceBremForDecay(0/*vpar[0]*/,vpar[1]);
-	if(vpar.size()==3)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2]);
-	if(vpar.size()==4)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3]);
-	if(vpar.size()==5)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4]);
-	if(vpar.size()==6)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5]);
-	if(vpar.size()==7)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6]);
-	if(vpar.size()==8)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7]);
-	if(vpar.size()==9)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8]);
-	if(vpar.size()==10) Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8],vpar[9]);
+        if(vpar.size()==1)  Photospp::Photos::suppressBremForDecay(0,vpar[0]);
+        if(vpar.size()==2)  Photospp::Photos::suppressBremForDecay(0/*vpar[0]*/,vpar[1]);
+        if(vpar.size()==3)  Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2]);
+        if(vpar.size()==4)  Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3]);
+        if(vpar.size()==5)  Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4]);
+        if(vpar.size()==6)  Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5]);
+        if(vpar.size()==7)  Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6]);
+        if(vpar.size()==8)  Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7]);
+        if(vpar.size()==9)  Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8]);
+        if(vpar.size()==10) Photospp::Photos::suppressBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8],vpar[9]);
       }
     }
-    if(curSet=="forceMass"){
-      std::vector<std::string> v = cfgAlgorithm.getParameter<std::vector<std::string> >(curSet);
+
+    if(curSet=="forceBremForBranch"){
+      edm::ParameterSet cfg = fPSet->getParameter<edm::ParameterSet>(curSet);
+      std::vector<std::string> v = cfg.getParameter<std::vector<std::string> >("parameterSets");
       for(unsigned int i=0;i<v.size();i++){
 	std::string vs = v[i];
-	edm::ParameterSet cfg = cfgAlgorithm.getParameter<edm::ParameterSet>(vs);
+	std::vector<int> vpar = cfg.getParameter<std::vector<int> >(vs);
+	if(vpar.size()==1)  Photospp::Photos::forceBremForBranch(0,vpar[0]);
+	if(vpar.size()==2)  Photospp::Photos::forceBremForBranch(0/*vpar[0]*/,vpar[1]);
+	if(vpar.size()==3)  Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2]);
+	if(vpar.size()==4)  Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3]);
+	if(vpar.size()==5)  Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4]);
+	if(vpar.size()==6)  Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5]);
+	if(vpar.size()==7)  Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6]);
+	if(vpar.size()==8)  Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7]);
+	if(vpar.size()==9)  Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8]);
+	if(vpar.size()==10) Photospp::Photos::forceBremForBranch(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8],vpar[9]);
+      }
+    }
+    if(curSet=="forceBremForDecay"){
+      edm::ParameterSet cfg = fPSet->getParameter<edm::ParameterSet>(curSet);
+      std::vector<std::string> v = cfg.getParameter<std::vector<std::string> >("parameterSets");
+      for(unsigned int i=0;i<v.size();i++){
+	std::string vs = v[i];
+	std::vector<int> vpar = cfg.getParameter<std::vector<int> >(vs);
+        if(vpar.size()==1)  Photospp::Photos::forceBremForDecay(0,vpar[0]);
+        if(vpar.size()==2)  Photospp::Photos::forceBremForDecay(0/*vpar[0]*/,vpar[1]);
+        if(vpar.size()==3)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2]);
+        if(vpar.size()==4)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3]);
+        if(vpar.size()==5)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4]);
+        if(vpar.size()==6)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5]);
+        if(vpar.size()==7)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6]);
+        if(vpar.size()==8)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7]);
+        if(vpar.size()==9)  Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8]);
+        if(vpar.size()==10) Photospp::Photos::forceBremForDecay(vpar[0],vpar[1],vpar[2],vpar[3],vpar[4],vpar[5],vpar[6],vpar[7],vpar[8],vpar[9]);
+      }
+    }
+
+    if(curSet=="forceMass"){
+      edm::ParameterSet cfg = fPSet->getParameter<edm::ParameterSet>(curSet);
+      std::vector<std::string> v = cfg.getParameter<std::vector<std::string> >("parameterSets");
+      for(unsigned int i=0;i<v.size();i++){
+	std::string vs = v[i];
 	std::vector<double> vpar = cfg.getParameter<std::vector<double> >(vs);
 	if(vpar.size()==2) Photospp::Photos::forceMass((int)vpar[0], vpar[1]);
       }
@@ -116,8 +152,8 @@ void PhotosppInterface::init(){
   // implement options set via c++ 
   if(fOnlyPDG>=0){
     Photospp::Photos::suppressAll();
-    Photospp::Photos::forceBremForDecay(0,fOnlyPDG);
-    Photospp::Photos::forceBremForDecay(0,-1*fOnlyPDG);
+    Photospp::Photos::forceBremForBranch(0,fOnlyPDG);
+    Photospp::Photos::forceBremForBranch(0,-1*fOnlyPDG);
   }
   if(fAvoidTauLeptonicDecays){
     Photospp::Photos::suppressBremForDecay(3, 15, 16, 11, -12);
@@ -125,6 +161,8 @@ void PhotosppInterface::init(){
     Photospp::Photos::suppressBremForDecay(3, 15, 16, 13, -14);
     Photospp::Photos::suppressBremForDecay(3, -15, -16, -13, -14);
   }
+  std::cout << "PhotosppInterface::init done" << std::endl;
+  Photospp::Photos::iniInfo();
   fIsInitialized = true; 
   return;
 }
